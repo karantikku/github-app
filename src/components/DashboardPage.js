79637624de8fcb4f9b7d318b386e4c9765f464e2
Karan Tikku <karantikku7@gmail.com>
2018-12-28 22:04:40 +0530

    implemented 2 column layout
    implemented 2 column layout

--- src/components/DashboardPage.js
+++ src/components/DashboardPage.js
@@ -3,11 +3,17 @@ import UserInfo from "./UserInfo";
 import RepositoriesBlock from "./RepositoriesBlock";
 import ActivityList from "./ActivityList";
 export default () => (
-  <div>
-    <h1>
-      <UserInfo />
-      <RepositoriesBlock />
-      <ActivityList />
-    </h1>
+  <div className="wrapper">
+    <div className="row">
+      <div class="column">
+        <UserInfo />
+      </div>
+      <div class="column">
+        <div className="info-block">
+          <RepositoriesBlock />
+          <ActivityList />
+        </div>
+      </div>
+    </div>
   </div>
 );
