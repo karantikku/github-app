1fccd257c85d633edc32e38be07e58499b4bdfcc
tikkuk <karantikku7@gmail.com>
2018-12-29 00:43:18 +0530

    implemented login

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import LoginPage from '../components/LoginPage';
-import NavBar from '../components/NavBar';
 import DashboardPage from '../components/DashboardPage';
 import RepositoriesListPage from '../components/RepositoriesListPage';
 import NotFoundPage from '../components/NotFoundPage';
@@ -9,7 +8,6 @@ import NotFoundPage from '../components/NotFoundPage';
 const AppRouter = () => (
   <BrowserRouter>
     <div>
-      <NavBar />
       <Switch>
         <Route path="/" component={LoginPage} exact={true} />
         <Route path="/dashboard" component={DashboardPage} />
